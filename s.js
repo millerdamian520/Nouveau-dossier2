b158b605b4775de2cb09e2320e8e721f4ff5f393
millerdamian520 <155296415+millerdamian520@users.noreply.github.com>
2024-02-26 13:37:18 +0100

    Add files via upload

---
+++ s.js
@@ -0,0 +1,45 @@
+function()
+var mainEl = document.getElementById('main-paragraph');
+console.log('el:', mainEl);
+
+var elClassName = mainEl.getAttribute('class');
+console.log('elClassName:', elClassName);
+
+var withoutStyleEl = document.getElementById('without-style');
+console.log('el:', withoutStyleEl);
+
+var withoutStyleElClassName = withoutStyleEl.getAttribute('class');
+console.log('withoutStyleElClassName:', withoutStyleElClassName);
+
+var contents = document.getElementById('main').innerHTML;
+   console.log('contents', contents);
+
+function {
+var num = 0;
+console.log(num)
+num ++
+console.log(num)
+num --
+console.log(num)
+}
+
+function verif()
+{
+    {
+if("this nom<8")
+return false;
+    }
+{
+if ("this prenom>8")
+returnn false;
+}
+{
+    if("this email")
+    return false;
+}
+{
+    if ("this telephone")
+    return false;
+}
+
+}
